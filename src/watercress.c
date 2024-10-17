47a947727c7add2d3f0f525d1ddbd79d1b532158
Vihaan Jim <vsj314@icloud.com>
2024-10-17 14:35:10 -0400

    debugging of hashmap

--- src/watercress.c
+++ src/watercress.c
@@ -1,5 +1,53 @@
 // c99
 #include <stdlib.h>
+#include "utils.h"
+#include "intermediate.h"
 int main(int argc, char** argv) {
+    /* HashMap *map = hashmap_create(&hashstr, &hashstr2, &streq); */
+    
+    /* hashmap_set(map, (void *)strmove("hello"), (void *)strmove("world")); */
+    /* hashmap_set(map, (void *)strmove("hello2"), (void *)strmove("world2")); */
+    /* hashmap_set(map, (void *)strmove("ahoy"), (void *)strmove("earth")); */
+    /* hashmap_set(map, (void *)strmove("what's up"), (void *)strmove("planet")); */
+
+    /* printf("%s\n", (char *)hashmap_get(map, (void *)strmove("hello"))); */
+    /* printf("%s\n", (char *)hashmap_get(map, (void *)strmove("ahoy"))); */
+
+    /* hashmap_set(map, (void *)strmove("jkjkdsf"), (void *)strmove("wieurqiure")); */
+    /* hashmap_set(map, (void *)strmove("uiweuri"), (void *)strmove("cvxzm,cv")); */
+    /* hashmap_set(map, (void *)strmove("nmncxvmn"), (void *)strmove("uiwerfds")); */
+    /* hashmap_set(map, (void *)strmove("izuifx"), (void *)strmove("nrmenwqrm")); */
+    /* hashmap_set(map, (void *)strmove("uixcuviz"), (void *)strmove("uijviox")); */
+    /* hashmap_set(map, (void *)strmove("jkfdjsio"), (void *)strmove("jksjdkfweqer")); */
+
+    /* printf("%s\n", (char *)hashmap_get(map, (void *)strmove("hello"))); */
+    /* printf("%s\n", (char *)hashmap_get(map, (void *)strmove("ahoy"))); */
+
+    /* hashmap_set(map, (void *)strmove("hello"), (void *)strmove("schartman")); */
+    /* printf("%s\n", (char *)hashmap_get(map, (void *)strmove("hello"))); */
+
+    /* hashmap_set(map, (void *)5, (void *)7); */
+    /* hashmap_set(map, (void *)10, (void *)8); */
+    /* hashmap_set(map, (void *)22, (void *)9); */
+
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)5))); */
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)10))); */
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)22))); */
+    
+    /* hashmap_set(map, (void *)22, (void *)10); */
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)22))); */
+    
+    /* hashmap_set(map, (void *)1, (void *)8); */
+    /* hashmap_set(map, (void *)2, (void *)8); */
+    /* hashmap_set(map, (void *)3, (void *)10); */
+    /* hashmap_set(map, (void *)4, (void *)8); */
+    /* hashmap_set(map, (void *)6, (void *)8); */
+    /* hashmap_set(map, (void *)7, (void *)10); */
+
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)5))); */
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)10))); */
+    /* printf("%ld\n", (long)(hashmap_get(map, (void *)22))); */
+    
+    /* hashmap_destroy(map); */
     return EXIT_SUCCESS;
 }
